5ca461c6e7d2a1630b1b34f823d0ad13438c2a32
Edbert Cheng <49933772+urbandesign09@users.noreply.github.com>
2020-10-16 18:17:24 -0500

    Background Update

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import Clock from "../container/Clock";
 import Title from "../components/Title";
 import Credit from "../container/Credit";
 import "./styles.css";
+import memegirl from "../assets/memegirl.jpg";
 
 //create a light and dark mode here
 export default function App() {
+  const ImgStr = 'url("' + memegirl + '")';
+  console.log(ImgStr);
   return (
-    <div className="App">
-      <Title />
-      <Clock />
-      <Credit />
+    <div className="App" style={{ backgroundImage: ImgStr }}>
+      <div className="Blank-Side"></div>
+      <div className="Function-Side">
+        <Title />
+        <Clock />
+        <Credit />
+      </div>
     </div>
   );
 }
